c163f0b8e91ed4d507e28624b57a2aee11de9323
Artem Dubovov <artem.dubovov@bk.ru>
2025-11-30 01:34:11 +0300

    feat: add from page and components ui

---
+++ src/pages/fromPage/FormPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./components/From/Form";
+
+function FormPage(){
+    return(
+        <div className="page">
+            <Form />
+        </div>
+    )
+}
+
+export default FormPage;
